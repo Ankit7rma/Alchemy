4ef91fe882b3eb013489314eef93d472ad458002
Ankit Sharma <ankitsharma290101@gmail.com>
2024-11-11 03:44:51 +0530

    Gas Price

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { Network, Alchemy } = require("alchemy-sdk");
 
 const settings = {
-  apiKey: "Demo",
+  apiKey: "Demo", // Replace with App Api
   network: Network.ETH_MAINNET,
 };
 
@@ -9,7 +9,9 @@ const alchemy = new Alchemy(settings);
 
 async function getBlock() {
   const latestBlock = await alchemy.core.getBlockNumber();
+  const gasPrice = await alchemy.core.getGasPrice();
   console.log("The latest block number is", latestBlock);
+  console.log("The gas Price", gasPrice);
 }
 
 getBlock();

08b5ab74be25cf15bcafcfc98b1704a9ba75017b
Ankit Sharma <ankitsharma290101@gmail.com>
2024-11-18 15:50:08 +0530

    SignMessage

--- AlchemyEthereumBootcamp/week1/public-key-cryptography/hashMessage/index.js
+++ AlchemyEthereumBootcamp/week1/public-key-cryptography/hashMessage/index.js
@@ -4,5 +4,7 @@ const { utf8ToBytes } = require("ethereum-cryptography/utils");
 function hashMessage(message) {
   const bytes = utf8ToBytes(message);
   const hash = keccak256(bytes);
-  console.log(hash);
+  return hash;
 }
+
+module.exports = hashMessage;

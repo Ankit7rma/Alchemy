e5fbd9eb8445f8ac75a9a05544034c91ea67d69b
Ankit Sharma <ankitsharma290101@gmail.com>
2024-11-19 17:49:06 +0530

    block hash

---
+++ AlchemyEthereumBootcamp/week1/BuildMiner/BlockHash.js
@@ -0,0 +1,30 @@
+const SHA256 = require("crypto-js/sha256");
+const TARGET_DIFFICULTY =
+  BigInt(0x0fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff);
+const MAX_TRANSACTIONS = 10;
+
+const mempool = [];
+const blocks = [];
+
+function addTransaction(transaction) {
+  // TODO: add transaction to mempool
+  return mempool.push(transaction);
+}
+
+function mine() {
+  // TODO: mine a block
+
+  let newBlock = { id: blocks.length };
+  const stringifyBlock = SHA256(JSON.stringify(newBlock));
+  newBlock.hash = stringifyBlock;
+  blocks.push(newBlock);
+}
+
+module.exports = {
+  TARGET_DIFFICULTY,
+  MAX_TRANSACTIONS,
+  addTransaction,
+  mine,
+  blocks,
+  mempool,
+};

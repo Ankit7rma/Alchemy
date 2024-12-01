527c06202c19837407bbe46067c9d6fe03332867
Ankit Sharma <ankitsharma290101@gmail.com>
2024-12-01 09:23:28 +0530

    contains

--- AlchemyEthereumBootcamp/week2 BlockChain Storage/treeDatastructure/Trie/contains.js
+++ AlchemyEthereumBootcamp/week2 BlockChain Storage/treeDatastructure/Trie/contains.js
@@ -22,6 +22,26 @@ class Trie {
 
     root.isWord = true;
   }
+  contains(word, root = this.root) {
+    let arr = word.split("");
+    if (arr.length === 0) {
+      return false;
+    }
+    let contains = true;
+
+    for (let i = 0; i < arr.length; i++) {
+      let letter = arr[i];
+      if (!root.isWord && root.children.hasOwnProperty(letter)) {
+        root = root.children[letter];
+      } else {
+        return false;
+      }
+    }
+    if (!root.isWord) {
+      return false;
+    }
+    return contains;
+  }
 }
 
 module.exports = Trie;
